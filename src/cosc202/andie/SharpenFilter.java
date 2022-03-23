d84e8e7628a3d954aa6328838fc251ee05a65d37
Chris Mairs <maich685@student.otago.ac.nz>
2022-03-24 10:30:17 +1300

    SharpenFilter

---
+++ src/cosc202/andie/SharpenFilter.java
@@ -0,0 +1,29 @@
+package cosc202.andie;
+
+import java.awt.image.*;
+
+public class SharpenFilter implements ImageOperation, java.io.Serializable {
+
+    SharpenFilter() {
+
+    }
+
+    @Override
+    public BufferedImage apply(BufferedImage input) {
+
+        float[] sharpen = {
+                0, -1 / 2, 0,
+                -1 / 2, 3, -1 / 2,
+                0, -1 / 2, 0
+        };
+
+        Kernel kernel = new Kernel(3, 3, sharpen);
+        ConvolveOp convOp = new ConvolveOp(kernel);
+        BufferedImage output = new BufferedImage(input.getColorModel(), input.copyData(null),
+                input.isAlphaPremultiplied(), null);
+        convOp.filter(input, output);
+
+        return output;
+    }
+
+}
