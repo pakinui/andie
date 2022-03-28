bb7f7fbd5f2e78620c748c00a87d0bb7a87b80be
Poppy Schlaadt <schpo592@student.otago.ac.nz>
2022-03-28 17:30:02 +1300

    tidied brighness class and added JavaDoc comments

--- src/cosc202/andie/BrightnessUp.java
+++ src/cosc202/andie/BrightnessUp.java
@@ -4,30 +4,28 @@ import java.awt.image.*;
 import java.util.*;
 import java.awt.*;
 
-
-
 /**
-contrast filter class
+ * <p>
+ * A class to change the brightness and/or contrast of an image in ANDIE.
+ * </p>
+ * //finish JavaDoc comments
+ * 
+ * @author Poppy Schlaadt
+ * @version 1.0
  */
-
  public class BrightnessUp implements ImageOperation, java.io.Serializable {
 
-    double one;
-    double two;
-    double three;
-    double four;
-    int fin;
-    double twotwo;
-
-   int contrast;//contrast
-    int brightness; // brightness
+    int contrast;
+    int brightness; 
     
      /**
-     constructor 
+      * constructor for BrightnessUp class
+      * @param bright1
+      * @param cont1
       */
-      BrightnessUp(int bright1, int cont1){
-        brightness = bright1;
-        contrast = cont1;
+      BrightnessUp(int b, int c){
+        brightness = b;
+        contrast = c;
     
        // System.out.println("brightness percentage: " + brightness);
        // System.out.println("Contrast percentage: " + contrast);
@@ -46,77 +44,60 @@ contrast filter class
           
         for (int y = 0; y < input.getHeight(); ++y) {
             for (int x = 0; x < input.getWidth(); ++x) {
-                int argb = input.getRGB(x, y);
 
-                Color col = new Color(input.getRGB(x,y));
+                //colour of the current pixel
+                Color colour = new Color(input.getRGB(x,y));
+
+                //alpha, red, green and blue values from the pixel colour(colour)
+                int a = colour.getAlpha();
+                int r = colour.getRed();
+                int g = colour.getGreen();
+                int b = colour.getBlue();
                 
-                int a = col.getAlpha();
-                int r = col.getRed();
-                int g = col.getGreen();
-                int b = col.getBlue();
-
-                if(x == 0 && y == 0) {
-                    System.out.println("red val: " + r);
-                    System.out.println("green val: " + g);
-                    System.out.println("blue val: " + b);
-                    System.out.println("argb: " + argb);
-                    System.out.println("b: " + brightness);
-                }
-              
+                //the new values for these colour values via the rgbMath() method
+                //adjusted for brightness and contrast
                 int newR = rgbMath(r);
                 int newG = rgbMath(g);
                 int newB = rgbMath(b);
 
+                //new colour from calculated values (and alpha which remains the same)
                 Color newARGB = new Color(newR,newG,newB,a);
-                int finCol = newARGB.getRGB();
 
-                input.setRGB(x, y, finCol);
-
-                if(x == 0 && y == 0) {
-                    
-
-                    System.out.println("\n\nred val: " + newR);
-                    System.out.println("green val: " + newG);
-                    System.out.println("blue val: " + newB);
-                    System.out.println("argb: " + argb);
-                    System.out.println("new argb: " + newARGB);
-
-                    System.out.println("Brightness: " + brightness);
-                   // System.out.println("one: " + one +"\ntwo: " + two + "\ntwotwo: " + twotwo + "\nthree: " + three + "\nfour: " + four + "\nfin: " + fin);
-                }
+                //get the integer value for the new colour and change this pixel to that colour
+                int finalColour = newARGB.getRGB();
+                input.setRGB(x, y, finalColour);
             }
         }
         
         return input;
     }
 
-    //brightness, contrast, value
+   /**
+    * a method to calculate the new R,G or B value for the brightness and contrast
+    * variables initalised in the constructor
+    * @param v value to be changed
+    * @return changed value
+    */
     public int rgbMath( int v){
-
-            
-            
-             double mid;
-
-             //((1+(c1/100))   *  (v-127.5))    + (127.5*  (1+(b1/100)) ) ((one*two)+(127.5*(three)))                          
-
-             one = (1+(contrast/100.0));
-             two = (v-127.5);
-             
-             mid = brightness/100.0;
-             three = (1+(mid));
-             four = ((one*two)+(127.5*(three)));
-            fin = (int) Math.round(four);
-            fin = inside(fin);
+        
+        //using the equation provided in the lab book
+        int fin = (int)Math.round(((1+(contrast/100.0)) * (v-127.5)) + (127.5 * (1+(brightness/100.0)))); 
+        fin = inside(fin);
 
         return fin;
     }
       
+    /**
+     * a method to test if an integer is between 0 and 255
+     * if x<0 x becomes 0 and if x>255 it becomes 255
+     * @param x integer to check
+     * @return checked integer between 0 and 255
+     */
+    public int inside(int x){
+        if(x<0) x =0;
+        if(x>255) x =255;
 
-      public int inside(int x){
-          if(x<0) x =0;
-          if(x>255) x =255;
-
-          return x;
-      }
+        return x;
+    }
 
  }
