a1375343b9a7ddc33f5e7b2d300352d8682303f5
Poppy Schlaadt <schpo592@student.otago.ac.nz>
2022-04-06 11:43:34 +1200

    got gaus filter to work I think

--- src/cosc202/andie/GussianFilter.java
+++ src/cosc202/andie/GussianFilter.java
@@ -3,6 +3,7 @@ package cosc202.andie;
 import java.util.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.*;
 
 import javax.swing.*;
 
@@ -17,11 +18,15 @@ public class GussianFilter implements ImageOperation, java.io.Serializable {
     int sizey;
     int xmiddle = (radius - 1);
     int ymiddle = (radius - 1);
+    int size; // number of elements in kernel
+    int length; // length of edges
 
     GussianFilter(int radius) {
         this.radius = radius;
         rad = (double) radius; // use a double for radius so it works in the equations with doubles an floats
         variance = (rad/3);
+        size = (2*radius + 1) * (2*radius+1);
+        length = (2*radius+1);
     }
 
     /*
@@ -55,41 +60,88 @@ public class GussianFilter implements ImageOperation, java.io.Serializable {
 
     }
 
+
+    float[] arrFill(float[] arr){
+
+        int index = 0;
+        int mid = radius+1;
+        for(int i = (radius*-1) ; i <= radius ; i++){
+            for(int ii = (radius*-1) ; ii <= radius ; ii++){
+                System.out.println("i: " + i + " | ii: " + ii);
+
+                int iMid = i;// length from middle i
+                int iiMid = ii;// length from middle ii
+                if(i < 0) iMid = i*-1;
+                if(ii < 0) iiMid = ii*-1;
+                System.out.println("iP: " + iMid + " | iiP: " + iiMid);
+                arr[index] = GussianEqaution(iMid, iiMid, variance); 
+                System.out.println("arr: " + arr[index]);
+                index++;
+            }
+
+        }
+        //these are the right numbers i think they match the numbers from lab book (figure 2,3)
+        float sum = 0;
+        for(float a : arr){
+            sum += a;
+        }
+        System.out.println("sum: " + sum);
+        float[] fin = new float[arr.length];
+        for(int f = 0; f < fin.length ; f++){
+            fin[f] = arr[f]/sum;
+        }
+
+        return fin;
+    }
+
     public BufferedImage apply(BufferedImage input) {
 
-        float[][] matrix = new float[2 * radius + 1][2 * radius + 1]; // creating a 2d float for the kernel
-        float[] flat = new float[(2 * radius + 1) * (2 * radius + 1)]; // creating a 1d float, we need to flatten 2d to
-                                                                       // 1d
-        int i = 0;
+        // float[][] matrix = new float[2 * radius + 1][2 * radius + 1]; // creating a 2d float for the kernel
+        // float[] flat = new float[(2 * radius + 1) * (2 * radius + 1)]; // creating a 1d float, we need to flatten 2d to
+        //                                                                // 1d
+        // int i = 0;
 
-        /*
-         * Creating a for loop that goes through the 2d float and adds the
-         * GussianEquation to each x,y point on the kernal.
-         */
-        for (int y = 0; y < matrix.length; y++) {
-            for (int x = 0; x < matrix[y].length; x++) {
-                matrix[y][x] = GussianEqaution(xmiddle + x - matrix.length / 2, ymiddle + y - matrix.length / 2,
-                        variance);
-            }
+        // /*
+        //  * Creating a for loop that goes through the 2d float and adds the
+        //  * GussianEquation to each x,y point on the kernal.
+        //  */
+        // for (int y = 0; y < matrix.length; y++) {
+        //     for (int x = 0; x < matrix[y].length; x++) {
+        //         matrix[y][x] = GussianEqaution(xmiddle + x - matrix.length / 2, ymiddle + y - matrix.length / 2,
+        //                 variance);
+        //     }
 
-        }
-        for (int y = 0; y < matrix.length; y++) {
-            for (int x = 0; x < matrix[y].length; x++) {
-                System.out.print(matrix[y][x] + " ");
+        // }
+        // for (int y = 0; y < matrix.length; y++) {
+        //     for (int x = 0; x < matrix[y].length; x++) {
+        //         System.out.print(matrix[y][x] + " ");
 
-            }
-            System.out.println();
-        }
+        //     }
+        //     System.out.println();
+        // }
 
-        /* Flattening the 2d float to a 1d float. */
-        for (int y = 0; y < matrix.length; y++) {
-            for (int x = 0; x < matrix[y].length; x++) {
-                flat[i] = matrix[y][x];
-                System.out.print(flat[i]);
-                i++;
-            }
-        }
-        return input;
+        // /* Flattening the 2d float to a 1d float. */
+        // for (int y = 0; y < matrix.length; y++) {
+        //     for (int x = 0; x < matrix[y].length; x++) {
+        //         flat[i] = matrix[y][x];
+        //         System.out.print(flat[i]);
+        //         i++;
+        //     }
+        // }
+
+            
+            float[] arr = new float[size];
+            arr = arrFill(arr);
+            System.out.println(Arrays.toString(arr));
+
+            Kernel kernel = new Kernel(length,length, arr);
+            ConvolveOp conOp = new ConvolveOp(kernel);
+            BufferedImage output = new BufferedImage(input.getColorModel(), input.copyData(null),
+                    input.isAlphaPremultiplied(), null);
+                    conOp.filter(input, output);
+            
+
+        return output;
 
     }
 
