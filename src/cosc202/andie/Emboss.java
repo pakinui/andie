967db5429d561ffbfa9c26c8375ddb884bc4ce42
Pippi Priestley King <pripi047@student.otago.ac.nz>
2022-05-25 17:02:43 +1200

    emboss filter finished

--- src/cosc202/andie/Emboss.java
+++ src/cosc202/andie/Emboss.java
@@ -1,12 +1,24 @@
 package cosc202.andie;
 
 import java.awt.image.BufferedImage;
-
-import java.awt.image.*;
-
+/**
+ * <p>
+ * ImageOperation to add emboss effects to the image and Sobel filters.
+ * </p>
+ * 
+ * <p>
+ * Method takes a kernel option given by the user and uses that kernel to apply one of the 8 emboss filters or 
+ * one of the sobel filters. 
+ * {@link BufferedImage} class. 
+ * </p>
+ * 
+ * @author Pippi Priestley King
+ * @version 1.0
+ */
     public class Emboss implements ImageOperation{
         int kernelNo = 0;
-        Kernel kernel = null;
+       
+    double [][] kernel = null;
        
         Emboss(int kernelNo){
         this.kernelNo = kernelNo;
@@ -15,62 +27,77 @@ import java.awt.image.*;
         }
 
     public BufferedImage apply(BufferedImage image){
-        
         setKernel(kernelNo);
-        // int p = image.getRGB(0,0);
-        // int a = (p>>24)&0xff;
-        // int r = (p>>16)&0xff;
-        // int g = (p>>8)&0xff;
-        // int b = p&0xff;
+        for (int row = 0; row < image.getHeight(); row ++) {
+            for (int col = 0; col < image.getWidth(); col++) {
+                
+                int red =0;
+                int green=0;
+                 int blue=0;
+                 int px=0;
+       
+              for (int row_offset = -1 ; row_offset < 2; row_offset++ ) {
+                for (int col_offset = -1 ; col_offset < 2 ; col_offset++ ) {
+       
+                  // subtract by half the kernel size to center the kernel
+                  // on the pixel in question
+                  int row_index = Math.min(Math.max(0, row + row_offset), image.getHeight()-1);
+                  int col_index = Math.min(Math.max(0, col + col_offset), image.getWidth()-1);
+       /*
+                  System.out.println(row_index);
+                  System.out.println(col_index);
+                  System.out.println(image.getHeight());
+                  System.out.println(image.getWidth());*/
+                  int p = image.getRGB(col_index, row_index);
 
-        System.out.println(kernelNo);
+                  int r = (p>>16)&0xff;
+                  int g = (p>>8)&0xff;
+                  int b = p&0xff;
+          
+                  red += r * kernel[row_offset+1][col_offset+1];
+                  green += g* kernel[row_offset+1][col_offset+1];
+                  blue +=b * kernel[row_offset+1][col_offset+1];
 
-    
-
-    ConvolveOp convOp = new ConvolveOp(kernel); // Constructs a ConvloveOp with the given Kernel.
-    BufferedImage output = new BufferedImage(image.getColorModel(), image.copyData(null),
-            image.isAlphaPremultiplied(), null); // Constructs a new BufferedImage with a ColorModel and Raster
-                                                 // and
-                                                 // is AlphaPremulitplied returned true.
-    convOp.filter(image, output);
-
+                
+                }
+                
+              }
+             // System.out.println(red + ", " + green + ", " + blue);
+              red+=127;
+              green+=127;
+              blue+=127;
+              int a = (image.getRGB(col, row)>>24)&0xff;
+              px = (a<<24) | (red<<16) | (green<<8) | blue;
+              image.setRGB(col, row, px);
+       
+            }
+          }
      
-    return output;
+    return image;
     }
 
     public void setKernel(int kernelNo){
-
         // depending what emboss filter they chose
     if(kernelNo== 1){
-        kernel = new Kernel(3, 3,
-    new float[] {0, 0, 0,1, 0, -1, 0, 0, 0}); }
-    if(kernelNo == 2){kernel = new Kernel(3, 3,
-    new float[] {1, 0, 0,0, 0, 0, 0, 0, 1}); }
+     kernel = new double [][] {{0, 0, 0},{1, 0, -1},{ 0, 0, 0}}; }
+    if(kernelNo == 2){kernel = new double[][]{{1, 0, 0},{0, 0, 0},{ 0, 0, 1}}; }
     if(kernelNo == 3){
-    kernel = new Kernel(3, 3,
-    new float[] {0, 1, 0,0, 0, 0, 0, -1, 0}); }
+    kernel = new double[][]{{0, 1, 0},{0, 0, 0},{0, -1, 0}}; }
     if(kernelNo == 4){
-    kernel = new Kernel(3, 3,
-    new float[] {0, 0, 1,0, 0,0, -1, 0, 0}); }
+    kernel = new double[][]{{0, 0, 1},{0, 0,0},{ -1, 0, 0}}; }
 
     if(kernelNo == 5){
-    kernel = new Kernel(3, 3,
-    new float[] {0, 0, 0,-1, 0, 1, 0, 0, 0}); }
+    kernel = new double[][]{{0, 0, 0},{-1, 0, 1},{ 0, 0, 0}}; }
     if(kernelNo == 6){
-    kernel = new Kernel(3, 3,
-    new float[] {-1, 0, 0,0, 0, 0, 0, 0, 1});}
+    kernel = new double[][]{{-1, 0, 0},{0, 0, 0},{0, 0, 1}};}
     if(kernelNo == 7){
-    kernel = new Kernel(3, 3,
-    new float[] {0, -1, 0,0, 0, 0, 0, 1, 0}); }
+    kernel = new double[][]{{0, -1, 0},{0, 0, 0},{0, 1, 0}}; }
     if(kernelNo == 8){
-    kernel = new Kernel(3, 3,
-    new float[] {0, 0, -1,0, 0, 0, 1, 0, 0});  }
+    kernel = new double[][]{{0, 0, -1},{0, 0, 0},{1, 0, 0}};  }
     if(kernelNo == 9){
-        kernel = new Kernel(3, 3,
-        new float[] {-1/2, -1, -1/2,0, 0, 0, 1/2, 1, 1/2});  }
+        kernel = new double[][]{{(-1/2), -1, -(1/2)},{0, 0, 0},{1/2, 1, (1/2)}};  }
     if(kernelNo == 10){
-            kernel = new Kernel(3, 3,
-            new float[] {-1/2, 0, 1/2,-1, 0, 1, -1/2, 0, 1/2});  }
+            kernel = new double[][]{{-(1/2), 0, (1/2)},{-1, 0, 1},{-(1/2), 0, (1/2)}};  }
 
     }
 }
